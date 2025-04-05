6c53ccca562b7204cdb4d357f59a39aa36a4c3fb
Shreyas Dankhade <85983813+ShreyasDankhade@users.noreply.github.com>
2025-04-05 16:15:26 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_KEY = "952bef2012b3882524c8e5f5b4f6d17a";
+const API_KEY = "add_your_api_key";
 const BASE_URL = "https://api.themoviedb.org/3";
 
 // async is used to get the data & it'll take a second to get the data.
